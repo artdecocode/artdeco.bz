568c41800aaf06b162cb0b28ac58cb619694513f
Anton <anton@adc.sh>
2018-05-18 19:08:36 +0100

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -29,23 +29,23 @@ export default {
     async function() {
       return async (ctx, next) => {
         ctx.setViewport('width=device-width, initial-scale=1, shrink-to-fit=no')
-        ctx.addCss(production ? [
+        ctx.addCss(production ? [[
           'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css',
           'sha256-Md8eaeo67OiouuXAi8t/Xpd8t2+IaJezATVTWbZqSOw=',
           'anonymous',
-        ] : 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css')
+        ]] : 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css')
 
-        ctx.addScript(production ? [
+        ctx.addScript(production ? [[
           'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js',
           'sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=',
           'anonymous',
-        ] : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.js')
+        ]] : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.js')
 
-        ctx.addScript(production ? [
+        ctx.addScript(production ? [[
           'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js',
           'sha256-xaF9RpdtRxzwYMWg4ldJoyPWqyDPCRD0Cv7YEEe6Ie8=',
           'anonymous',
-        ] : 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js')
+        ]] : 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/js/bootstrap.min.js')
 
 
         await next()
@@ -58,15 +58,16 @@ export default {
       return async (ctx, next) => {
         ctx.addScript(production ? [
           [
-            'https://cdnjs.cloudflare.com/ajax/libs/react/16.3.2/cjs/react.production.min.js', // 'https://cdnjs.cloudflare.com/ajax/libs/react/16.3.2/umd/react.development.js',
-            'sha256-oTABb6ke88mAAjkyf2gQcEnhdaQiE28BBX//lMeFpUY=', // 'sha256-s4xjpfNLyFo6uEJP3/OXxMoi8LZBCaB2weyDvhAbs1c=',
+            'https://cdnjs.cloudflare.com/ajax/libs/react/16.3.2/umd/react.production.min.js',
+            'sha256-R9mHnFtrqNBSv0u7IGrwrn8TxfZaWD0UjHo9xvqDHSM=',
             'anonymous',
           ],
         ] : 'https://cdnjs.cloudflare.com/ajax/libs/react/16.3.2/umd/react.development.js')
+
         ctx.addScript(production ? [
           [
-            'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.3.2/umd/react-dom.production.min.js', // 'https://cdnjs.cloudflare.com/ajax/libs/react/16.3.2/umd/react.development.js',
-            'sha256-lrhKJaWYTDnqslOwj/B8fz6bqehISA64woQRLqBKDbA=', // 'sha256-s4xjpfNLyFo6uEJP3/OXxMoi8LZBCaB2weyDvhAbs1c=',
+            'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.3.2/umd/react-dom.production.min.js',
+            'sha256-lrhKJaWYTDnqslOwj/B8fz6bqehISA64woQRLqBKDbA=',
             'anonymous',
           ],
         ] : 'https://cdnjs.cloudflare.com/ajax/libs/react-dom/16.3.2/umd/react-dom.development.js')
